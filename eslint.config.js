c69793366522eff532884bd6d93f1b9ea0abd6fd
Basix <basix@basix.tech>
2025-05-12 06:54:39 +0900

    Configure editor tools

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  ...eslintPluginAstro.configs.recommended,
+  tseslint.configs.recommended,
+]);
