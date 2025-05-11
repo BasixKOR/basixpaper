68a601a1d13bda71942c56172c9a521f085e9f3d
Basix <basix@basix.tech>
2025-05-12 07:27:21 +0900

    Add React

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,21 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: { js },
     extends: ['js/recommended'],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: { globals: globals.browser },
   },
   ...eslintPluginAstro.configs.recommended,
   tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
+
